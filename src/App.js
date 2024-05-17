1e9d084a679594210f7b37d5b016c458bd76668c
Myoung Hun <myeonghun1212@gmail.com>
2024-05-17 13:16:57 +0900

    Refactor App component and SearchBar component
    - Refactor App component to use class-based component instead of functional component
    - Update grid template rows and columns in App component
    - Add state and updateData method to App component
    - Refactor SearchBar component to use class-based component instead of functional component
    - Add advancedSearch state and updateAdvancedSearch method to SearchBar component
    - Update SearchBar component UI and functionality

--- src/App.js
+++ src/App.js
@@ -2,47 +2,75 @@ import React from 'react';
 import {
   ChakraProvider,
   theme,
-  Grid, GridItem, Center
+  Grid,
+  GridItem,
+  Center,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 import SearchBar from './SearchBar';
 
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Grid
-        templateAreas={`"header header"
-                  "SearchBar SearchBar"
-                  "nav main"
-                  "footer footer"`}
-        gridTemplateRows={'50px 1fr 5fr 50px'}
-        gridTemplateColumns={'150px 1fr'}
-        h="100vh"
-        gap="1"
-        color="blackAlpha.700"
-        fontWeight="bold"
-      >
-        <GridItem pl="2" bg="orange.300" area={'header'}>
-          Header
-        </GridItem>
-        <GridItem pl="2" area={'SearchBar'}>
-          <Center h='100%' axis='both'>
-          <SearchBar/>
-          </Center>
-          </GridItem>
-        <GridItem pl="2" bg="pink.300" area={'nav'}>
-          Nav
-        </GridItem>
-        <GridItem pl="2" bg="green.300" area={'main'}>
-          Main
-        </GridItem>
-        <GridItem pl="2" bg="blue.300" area={'footer'}>
-          Footer
-        </GridItem>
-      </Grid>
-    </ChakraProvider>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      advancedSearch: false,
+    };
+  }
 
-export default App;
+  updateData = (target, value) => {
+    this.setState({[target]: value});
+  };
+
+
+
+  render() {
+    return (
+      <ChakraProvider theme={theme}>
+        <Grid
+          // templateAreas={`"header header"
+          //           "SearchBar SearchBar"
+          //           "nav main"
+          //           "footer footer"`}
+          templateRows="50px 1fr 2fr 5fr 50px"
+          templateColumns="150px 1fr"
+          // gridTemplateRows={'50px 1fr 5fr 50px'}
+          // gridTemplateColumns={'150px 1fr'}
+          h="100vh"
+          gap="1"
+          color="blackAlpha.700"
+          fontWeight="bold"
+        >
+          <GridItem
+            colSpan={2}
+            rowSpan={1}
+            pl={2}
+            bg="orange.300"
+            area={'header'}
+          >
+            Header
+          </GridItem>
+          <GridItem colSpan={2} rowSpan={this.state.advancedSearch ? 2 : 1} pl={2} area={'SearchBar'}>
+            <Center h="100%" axis="both">
+              <SearchBar updateData={this.updateData} advancedSearch={this.state.advancedSearch} />
+            </Center>
+          </GridItem>
+          <GridItem colSpan={1} rowSpan={this.state.advancedSearch ? 1 : 2} pl="2" bg="pink.300" area={'nav'}>
+            Nav
+          </GridItem>
+          <GridItem colSpan={1} rowSpan={this.state.advancedSearch ? 1 : 2} pl="2" bg="green.300" area={'main'}>
+            Main
+          </GridItem>
+          <GridItem
+            colSpan={2}
+            rowSpan={1}
+            pl="2"
+            bg="blue.300"
+            area={'footer'}
+          >
+            Footer
+          </GridItem>
+        </Grid>
+      </ChakraProvider>
+    );
+  }
+}
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Box,
   Spacer,
@@ -9,54 +10,71 @@ import {
 } from '@chakra-ui/react';
 // import { GoSearch } from 'react-icons/go';
 import { ChevronUpIcon, ChevronDownIcon, SearchIcon } from '@chakra-ui/icons';
-function SearchBar() {
-  return (
-    <Flex w="100%">
-      <Spacer />
-      <form style={{ width: '100%' }}
-        onSubmit={() => {
-          window.alert('submit');
-        }}
-      >
-        <Center>
-          <Input
-            placeholder="검색어를 입력하세요"
-            borderColor={'blue.300'}
-            focusBorderColor="blue.300"
-            size="lg"
-            width="34%"
-            height="4.8vmin"
-            maxWidth="100%"
-            mr="1.3vw"
-            fontSize="2vmin"
-          />
-          <IconButton
-            variant="outline"
-            colorScheme="blue"
-            aria-label="Search database"
-            h="4.8vmin"
-            w="4.8vmin"
-            mr="1.3vw"
-            icon={<SearchIcon />}
-            type="submit"
-          />
-          <Button
-            rightIcon={<ChevronDownIcon />}
-            colorScheme="blue"
-            variant="solid"
-            size="lg"
-            width="12.5vmin"
-            height="4.8vmin"
-            fontSize="1.5vmin"
-          >
-            상세검색
-          </Button>
-        </Center>
-      </form>
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateAdvancedSearch = this.updateAdvancedSearch.bind(this);
+  }
 
-      <Spacer />
-    </Flex>
-  );
+  updateAdvancedSearch() {
+    this.props.updateData('advancedSearch', !this.props.advancedSearch);
+  }
+
+  render() {
+    return (
+      <Flex w="100%">
+        <Spacer />
+        <form
+          style={{ width: '100%' }}
+          onSubmit={() => {
+            window.alert('submit');
+          }}
+          action="http://localhost:3000/search"
+          method="get"
+        >
+          <Center>
+            <Input
+              name="keyword"
+              placeholder="검색어를 입력하세요"
+              borderColor={'blue.300'}
+              focusBorderColor="blue.300"
+              size="lg"
+              width="34%"
+              height="4.8vmin"
+              maxWidth="100%"
+              mr="1.3vw"
+              fontSize="2vmin"
+            />
+            <IconButton
+              variant="outline"
+              colorScheme="blue"
+              aria-label="Search database"
+              h="4.8vmin"
+              w="4.8vmin"
+              mr="1.3vw"
+              icon={<SearchIcon />}
+              type="submit"
+            />
+            
+            <Button
+              rightIcon={
+                this.props.advancedSearch ? <ChevronUpIcon /> : <ChevronDownIcon />
+              }
+              colorScheme="blue"
+              variant="solid"
+              size="lg"
+              width="12.5vmin"
+              height="4.8vmin"
+              fontSize="1.5vmin"
+              onClick={this.updateAdvancedSearch}
+            >
+              상세검색
+            </Button>
+          </Center>
+        </form>
+
+        <Spacer />
+      </Flex>
+    );
+  }
 }
-
-export default SearchBar;
